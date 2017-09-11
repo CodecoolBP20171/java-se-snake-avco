89adba92e248196b431588402583ae35c2078fdb
peterormai <ormaipeter.fx@gmail.com>
2017-09-11 15:35:50 +0200

    SetLongerPowerup class first version implemented

---
+++ src/com/codecool/snake/entities/powerups/SetLongerPowerup.java
@@ -0,0 +1,36 @@
+package com.codecool.snake.entities.powerups;
+
+import com.codecool.snake.Globals;
+import com.codecool.snake.entities.GameEntity;
+import com.codecool.snake.entities.Interactable;
+import com.codecool.snake.entities.snakes.SnakeHead;
+import javafx.scene.layout.Pane;
+
+import java.util.Random;
+
+public class SetLongerPowerup extends GameEntity implements Interactable {
+
+    // a simple powerup that makes the snake grow TODO make other powerups
+
+    public SetLongerPowerup(Pane pane) {
+        super(pane);
+        setImage(Globals.powerupLonger);
+        pane.getChildren().add(this);
+
+        Random rnd = new Random();
+        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
+        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+    }
+
+    @Override
+    public void apply(SnakeHead snakeHead) {
+        snakeHead.addPart(10);
+        snakeHead.changeHealth(20);
+        destroy();
+    }
+
+    @Override
+    public String getMessage() {
+        return "Got power-up 10 :)";
+    }
+}
