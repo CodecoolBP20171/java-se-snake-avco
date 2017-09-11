767a213e34116a99f5966496c4757968b11f935a
Gergely Németh <ngerle@gmail.com>
2017-09-11 21:23:18 +0200

    Add Laser class

---
+++ src/com/codecool/snake/entities/weapons/Laser.java
@@ -0,0 +1,7 @@
+package com.codecool.snake.entities.weapons;
+
+import com.codecool.snake.entities.Animatable;
+import com.codecool.snake.entities.GameEntity;
+
+public class Laser extends GameEntity implements Animatable {
+}
