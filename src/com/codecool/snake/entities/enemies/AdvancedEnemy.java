811d64dee32ab187d70c9cf09ef29b7ecdece28c
matebalogh11 <b.mate11@gmail.com>
2017-09-12 10:10:27 +0200

    advancedenemy class was created

--- src/com/codecool/snake/entities/enemies/AdvancedEnemy.java
+++ src/com/codecool/snake/entities/enemies/AdvancedEnemy.java
@@ -1,35 +1,23 @@
 package com.codecool.snake.entities.enemies;
 
-import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
-import com.codecool.snake.entities.Animatable;
 import com.codecool.snake.Utils;
+import com.codecool.snake.entities.Animatable;
+import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.Interactable;
 import com.codecool.snake.entities.snakes.SnakeHead;
-import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
 
-import java.util.Random;
+public class AdvancedEnemy extends GameEntity implements Animatable, Interactable {
 
-// a simple enemy TODO make better ones.
-public class SimpleEnemy extends GameEntity implements Animatable, Interactable {
+    private SnakeHead snake;
+    private int damage = 10;
 
-    private Point2D heading;
-    private static final int damage = 10;
-
-    public SimpleEnemy(Pane pane) {
+    public AdvancedEnemy(Pane pane, SnakeHead snake) {
         super(pane);
-
+        this.snake = snake;
         setImage(Globals.simpleEnemy);
         pane.getChildren().add(this);
-        int speed = 1;
-        Random rnd = new Random();
-        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
-        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
-
-        double direction = rnd.nextDouble() * 360;
-        setRotate(direction);
-        heading = Utils.directionToVector(direction, speed);
     }
 
     @Override
@@ -37,6 +25,10 @@ public class SimpleEnemy extends GameEntity implements Animatable, Interactable 
         if (isOutOfBounds()) {
             destroy();
         }
+        // Advanced enemy skill - following the player - SnakeHead object is needed.
+        double direction = Math.toDegrees(Math.atan2(snake.getY() - getY(), snake.getX() - getX()));
+        direction += 90;
+        javafx.geometry.Point2D heading = Utils.directionToVector(direction, 1);
 
         setX(getX() + heading.getX());
         setY(getY() + heading.getY());
