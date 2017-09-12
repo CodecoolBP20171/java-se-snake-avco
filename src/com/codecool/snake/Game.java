f41675a6f4c19289ff9e2a0d4bb0e774031bd145
hankoattila <hattilla4@gmail.com>
2017-09-12 14:48:43 +0200

    Game start fixed after the multiplayer merge

--- src/com/codecool/snake/Game.java
+++ src/com/codecool/snake/Game.java
@@ -8,38 +8,29 @@ import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 
-import java.security.Key;
 
 public class Game extends Pane {
 
     public void start() {
-        createSnakes();
-        createSimpleEnemy();
+        SnakeHead snake =  new SnakeHead(this, 100, 500, KeyCode.LEFT, KeyCode.RIGHT, KeyCode.UP);
+        SnakeHead snake2 =  new SnakeHead(this, 200, 500, KeyCode.A, KeyCode.D, KeyCode.W);
+
         createSimplePowerup();
 
         Globals.SimpleEnemies.add(Globals.simpleEnemy);
         Globals.SimpleEnemies.add(Globals.simpleEnemy1);
         Globals.SimpleEnemies.add(Globals.simpleEnemy2);
         Globals.SimpleEnemies.add(Globals.simpleEnemy3);
-        Globals.gameLoop = new GameLoop();
-        Globals.gameLoop.start();
-    }
-
-    private void createSnakes() {
-        new SnakeHead(this, 100, 500, KeyCode.LEFT, KeyCode.RIGHT, KeyCode.UP);
-        new SnakeHead(this, 200, 500, KeyCode.A, KeyCode.D, KeyCode.W);
-    }
-
-    private void createSimpleEnemy() {
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
-    }
 
         new SimpleEnemy(this, snake);
         new AdvancedEnemy(this, snake);
         new NotSoSimpleEnemy(this, snake);
+
+
+        Globals.gameLoop = new GameLoop();
+        Globals.gameLoop.start();
+    }
+
 
     private void createSimplePowerup() {
         new SimplePowerup(this);
