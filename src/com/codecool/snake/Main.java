d0e05972d15a12d42d67a89221f905c0dd05fe6c
hankoattila <hattilla4@gmail.com>
2017-09-13 14:20:06 +0200

    Create wall methode replaced from Main to Gui

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -1,21 +1,11 @@
 package com.codecool.snake;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-
-import javax.xml.bind.annotation.XmlElementDecl;
-import java.awt.*;
 
 public class Main extends Application {
 
@@ -36,22 +26,7 @@ public class Main extends Application {
         primaryStage.setTitle("Snake Game");
         Scene scene = new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT);
 
-        Rectangle test = new Rectangle(Globals.WINDOW_WIDTH , Globals.WINDOW_HEIGHT );
-        test.setStroke(Color.YELLOWGREEN);
-        test.setFill(Color.LIGHTSEAGREEN);
-        game.getChildren().add(test);
-        test.setStrokeWidth(20);
-
-        Line gate = new Line(5, Globals.WINDOW_HEIGHT / 2 - 50, 5, Globals.WINDOW_HEIGHT / 2 + 50);
-        gate.setFill(Color.RED);
-        gate.setStrokeWidth(10);
-        game.getChildren().add(gate);
-
-        Line gate2 = new Line(Globals.WINDOW_WIDTH -5 , Globals.WINDOW_HEIGHT / 2 - 50, Globals.WINDOW_WIDTH - 5, Globals.WINDOW_HEIGHT / 2 + 50);
-        gate2.setFill(Color.RED);
-        gate2.setStrokeWidth(10);
-        game.getChildren().add(gate2);
-
+        Gui.chooseTheme(game);
         primaryStage.setScene(scene);
         primaryStage.show();
 
