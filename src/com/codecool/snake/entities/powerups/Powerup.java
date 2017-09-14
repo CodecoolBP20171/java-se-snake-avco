8aae5c621ba9cb788bf89c98a96b020d9ff198fd
peterormai <ormaipeter.fx@gmail.com>
2017-09-14 14:57:57 +0200

    Set vald position to powerups to avoid to put them on the snakes

--- src/com/codecool/snake/entities/powerups/Powerup.java
+++ src/com/codecool/snake/entities/powerups/Powerup.java
@@ -1,5 +1,6 @@
 package com.codecool.snake.entities.powerups;
 
+import com.codecool.snake.Utils;
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
 import com.codecool.snake.entities.Interactable;
@@ -14,18 +15,31 @@ public abstract class Powerup extends GameEntity implements Interactable {
 
     private Queue<Vec2d> history = new LinkedList<>();
     private static int numberOfPowerups;
+    private int entitySize;
+    protected Random rand = new Random();
 
     public Powerup(Pane pane) {
         super(pane);
         pane.getChildren().add(this);
-
-        Random rnd = new Random();
-        int charSize = 40;
-        double xPos = (double) rnd.nextInt((int) (Globals.WINDOW_WIDTH - charSize) - charSize + 1) + charSize;
-        double yPos = (double) rnd.nextInt((int) (Globals.WINDOW_HEIGHT - charSize) - charSize + 1) + charSize;
-        setX(xPos);
-        setY(yPos);
+        setValidPosition();
         numberOfPowerups++;
+    }
+
+    public void setValidPosition() {
+        entitySize = 40;
+        boolean inValidPosition = true;
+        while (inValidPosition) {
+            double xPos = (double) rand.nextInt((int) (Globals.WINDOW_WIDTH - entitySize) - entitySize + 1) + entitySize;
+            double yPos = (double) rand.nextInt((int) (Globals.WINDOW_HEIGHT - entitySize) - entitySize + 1) + entitySize;
+            setX(xPos);
+            setY(yPos);
+            for (SnakeHead player: Globals.players) {
+                if (getBoundsInParent().intersects(player.getBoundsInParent())) {
+                    inValidPosition = false;
+                }
+            }
+            break;
+        }
     }
 
     @Override
